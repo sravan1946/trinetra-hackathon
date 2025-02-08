e28255925638d0e07d7e57bd93faf60c13dbd91a
sravan1946 <miniclip1956@gmail.com>
2025-02-08 18:00:57 +0530

    mangadex-dl added

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO
+import asyncio
+
+
+
+async def download_manga(manga_id: str, format: str = "raw"):
+    process = await asyncio.create_subprocess_exec(
+        "mangadex-dl", manga_id, "--path", "mangadex-dl_downloads", "-pbl", "stacked", "-f", format
+    )
+    await process.communicate()
+    socketio.emit("download_complete", {"manga_id": manga_id})
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
---
+++ utils.py
@@ -0,0 +1,40 @@
+from mangadex import Auth, Manga, Chapter
+from pprint import pprint
+from icecream import ic
+from typing import Optional, Literal
+import subprocess
+
+auth = Auth()
+manga = Manga(auth=auth)
+search = manga.get_manga_list(title="solo leveling")
+titles = [(manga.title, manga.manga_id) for manga in search]
+chapter = Chapter(auth=auth)
+pprint(titles)
+# pprint(search[1])
+chapter_list = chapter.get_chapter_list(manga=titles[1][1],limit=100,translatedLanguage=["en"],
+                                        volume=[1], chapter=[1])
+pprint(chapter_list)
+print([chap.chapter for chap in chapter_list])
+print(search[1].lastChapter)
+print(search[1].lastVolume)
+
+# setup subprocess and run the command mangadex-dl to download the file
+subprocess.run(["mangadex-dl", search[0].manga_id, "--path", "mangadex-dl_downloads", "-pbl", "stacked"])
+
+
+def get_manga_id_from_title(title: str) -> Optional[str]:
+    manga = Manga(auth=auth)
+    search = manga.get_manga_list(title=title)
+    manga_id = [manga.manga_id for manga in search if manga.lastChapter is not None and manga.lastVolume is not None]
+    return manga_id[0] if manga_id else None
+
+def get_manga_from_id(manga_id: str) -> Optional[Manga]:
+    manga = Manga(auth=auth)
+    search = manga.get_manga_list(manga_ids=[manga_id])
+    return search[0] if search else None
+
+
+VALID_FORMATS = Literal["raw","raw-volume","tachiyomi","pdf","pdf-volume"]
+def download_manga(manga_id: str, format: VALID_FORMATS = "raw") -> None:
+    subprocess.run(["mangadex-dl", manga_id, "--path", "mangadex-dl_downloads", "-pbl", "stacked", "-f", format])
+    
